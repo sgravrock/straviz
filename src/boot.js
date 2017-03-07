3d609e04a655d283bb3063cf544bb19d0b607e4d
Steve Gravrock <sdg@panix.com>
2017-03-06 20:36:35 -0800

    Fixed thumbnail view

--- src/boot.js
+++ src/boot.js
@@ -1,9 +1,9 @@
 (function () {
 	var map = document.getElementById("map");
-	var match = location.search.match(/^\?activity=([0-9]+)$/);
+	var match = location.search.match(/(^\?|&)activity=([0-9]+)$/);
 
 	if (match) {
-		new App(loadUrl, map, "#elevation-plot", "#speed-plot").start(match[1]);
+		new App(loadUrl, map, "#elevation-plot", "#speed-plot").start(match[2]);
 	} else {
 		alert("Missing activity parameter");
 	}
